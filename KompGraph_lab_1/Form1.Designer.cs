ba6cd0ce077ac059ae05f02c627a42cd855a07e1
Taras <vtaras15@gmail.com>
2018-09-26 19:45:46 +0300

    Добавьте файлы проекта.

---
+++ KompGraph_lab_1/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace KompGraph_lab_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.start_Button = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.bird_Release = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(798, 410);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // start_Button
+            // 
+            this.start_Button.Location = new System.Drawing.Point(351, 415);
+            this.start_Button.Name = "start_Button";
+            this.start_Button.Size = new System.Drawing.Size(75, 23);
+            this.start_Button.TabIndex = 1;
+            this.start_Button.Text = "Фейерверк";
+            this.start_Button.UseVisualStyleBackColor = true;
+            this.start_Button.Click += new System.EventHandler(this.start_Button_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // bird_Release
+            // 
+            this.bird_Release.Location = new System.Drawing.Point(474, 414);
+            this.bird_Release.Name = "bird_Release";
+            this.bird_Release.Size = new System.Drawing.Size(75, 23);
+            this.bird_Release.TabIndex = 2;
+            this.bird_Release.Text = "Коляска";
+            this.bird_Release.UseVisualStyleBackColor = true;
+            this.bird_Release.Click += new System.EventHandler(this.bird_Release_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bird_Release);
+            this.Controls.Add(this.start_Button);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button start_Button;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button bird_Release;
+    }
+}
+
---
+++ KompGraph_lab_1/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace KompGraph_lab_1
+{
+    public partial class Form1 : Form
+    {
+        Graphics gr,car;
+        Pen pen,car_Pen;
+        SolidBrush solidBrush,car_Brush;
+        Random rnd;
+        int rad;
+
+       
+       
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        void DrawCircle(int x, int y)
+        {
+            int xc, yc;
+            xc = x - rad;
+            yc = y - rad;
+            gr.FillEllipse(solidBrush, xc, yc, rad, rad);
+            gr.DrawEllipse(pen, xc, yc, rad, rad);
+
+
+        }
+
+        
+
+        private void start_Button_Click(object sender, EventArgs e)
+        {
+            
+            gr = pictureBox1.CreateGraphics();
+            pen = new Pen(Color.DarkGoldenrod);
+            solidBrush = new SolidBrush(Color.LemonChiffon);
+            rad = 30;
+            rnd = new Random();
+            int x, y;
+            for(int i = 0; i < 10; i++)
+            {
+                x = rnd.Next(pictureBox1.Width);
+                y = rnd.Next(pictureBox1.Height);
+                DrawCircle(x, y);
+                
+            }
+            timer1.Enabled = true;
+        
+
+        }
+
+        private void bird_Release_Click(object sender, EventArgs e)
+        {
+            car = pictureBox1.CreateGraphics();
+            car_Pen = new Pen(Color.Black, 3);
+            car_Brush = new SolidBrush(Color.RoyalBlue);
+            Pen p = new Pen(Color.Brown, 5);
+
+
+            while (true)
+            {
+                
+                for ( int i = pictureBox1.Left; i < pictureBox1.Right - 100; i++)
+                {
+
+                   
+                        car.DrawLine(p, 131 + i, 300, 150 + i, 250);
+                        car.DrawRectangle(car_Pen, 30 + i, 300, 100, 50);
+                        car.FillRectangle(car_Brush, 30 + i, 300, 100, 50);
+                        car.DrawRectangle(car_Pen, 30 + i, 235, 50, 65);
+                        car.FillRectangle(car_Brush, 30 + i, 235, 50, 65);
+                        car.DrawEllipse(new Pen(Color.Black, 2), 30 + i, 350, 30, 30);
+                        car.FillEllipse(new SolidBrush(Color.Salmon), 30 + i, 350, 30, 30);
+                        car.DrawEllipse(new Pen(Color.Black, 2), 100 + i, 350, 30, 30);
+                        car.FillEllipse(new SolidBrush(Color.Salmon), 100 + i, 350, 30, 30);
+                       
+                        
+
+                    if (i == pictureBox1.Right - 150)
+                    {
+                        car.DrawLine(p, 131 + i, 300, 150 + i, 250);
+                        car.DrawRectangle(car_Pen, 30 + i, 300, 100, 50);
+                        car.FillRectangle(car_Brush, 30 + i, 300, 100, 50);
+                        car.DrawRectangle(car_Pen, 30 + i, 235, 50, 65);
+                        car.FillRectangle(car_Brush, 30 + i, 235, 50, 65);
+                        car.DrawEllipse(new Pen(Color.Black, 2), 30 + i, 350, 30, 30);
+                        car.FillEllipse(new SolidBrush(Color.Salmon), 30 + i, 350, 30, 30);
+                        car.DrawEllipse(new Pen(Color.Black, 2), 100 + i, 350, 30, 30);
+                        car.FillEllipse(new SolidBrush(Color.Salmon), 100 + i, 350, 30, 30);
+                        Thread.Sleep(2);
+                        car.Clear(Color.White);
+                        /* for (int j = i; j > 0; j--)
+                         {
+
+                             car.DrawLine(p, 131 + j, 300, 150 + j, 250);
+                             car.DrawRectangle(car_Pen, 30 + j, 300, 100, 50);
+                             car.FillRectangle(car_Brush, 30 + j, 300, 100, 50);
+                             car.DrawRectangle(car_Pen, 30 + j, 235, 50, 65);
+                             car.FillRectangle(car_Brush, 30 + j, 235, 50, 65);
+                             car.DrawEllipse(new Pen(Color.Black, 2), 30 + j, 350, 30, 30);
+                             car.FillEllipse(new SolidBrush(Color.Salmon), 30 + j, 350, 30, 30);
+                             car.DrawEllipse(new Pen(Color.Black, 2), 100 + j, 350, 30, 30);
+                             car.FillEllipse(new SolidBrush(Color.Salmon), 100 + j, 350, 30, 30);
+                             Thread.Sleep(2);
+
+                             car.Clear(Color.White);
+                         }*/
+
+                    }
+                    Thread.Sleep(2);
+                    car.Clear(Color.White);
+
+                }
+            }
+        }
+                
+     
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            gr.Clear(Color.White);
+            int x, y;
+            for (int i = 0; i < 10; i++)
+            {
+                x = rnd.Next(pictureBox1.Width);
+                y = rnd.Next(pictureBox1.Height);
+                DrawCircle(x, y);
+            }
+        }
+        
+
+    }
+}
